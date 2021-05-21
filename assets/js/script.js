d1b437c2610ba178e8044cc0d1ef1cf622508884
Hiimus <hiimhanseneirik@gmail.com>
2021-05-21 16:01:45 +0000

    Added conditions to card flips and set timer when there is no match.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,35 @@
 const cards = document.querySelectorAll('.match-card');
 
-function flipCard(){
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
     this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // Second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // Do cards match?
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            // it's a match!
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            // not a match
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1000);
+                
+        } 
+        
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
