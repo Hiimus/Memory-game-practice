00304d4c3ecefe0a4b75e00a9ce743fccd117cc7
Hiimus <hiimhanseneirik@gmail.com>
2021-05-22 13:12:05 +0000

    Added code to prevent flipping more than two cards at a time, also added shuffle of cards.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,69 @@
 const cards = document.querySelectorAll('.match-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    this.classList.add('flip');
 
     if (!hasFlippedCard) {
         // first click
         hasFlippedCard = true;
         firstCard = this;
-    } else {
-        // Second click
-        hasFlippedCard = false;
-        secondCard = this;
 
-        // Do cards match?
+        return;
+    } 
+     // Second click
+    
+    secondCard = this;
+
+    checkForMatch();
+}
+
+
+function checkForMatch() {
+    // Do cards match?
         if (firstCard.dataset.framework === secondCard.dataset.framework) {
             // it's a match!
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
+            disableCards();
         } else {
             // not a match
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1000);
-                
+            unflipCards();  
         } 
-        
-    }
 }
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(function() {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+            resetBoard();
+        }, 1000);    
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    })
+})();
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
